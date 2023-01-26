2cbf5feefa97b818f1bb501fa55b24cdde3824da
LKyoona <438396211@qq.com>
2023-01-26 15:36:16 +0800

    Add files via upload

---
+++ 逗比拯救世界.py
@@ -0,0 +1,26 @@
+import requests
+import os
+
+if not os.path.exists('逗比图库'):
+    os.mkdir('逗比图库')
+else:
+    pass
+times = input('请输入下载数量单位(百):')
+for i in range(int(times)):
+    url = 'https://www.dbbqb.com/api/search/json?size=100'
+    headers = {
+        'Cookie': '_vc=96182aba-36d0-4375-82d6-785c1c27437f',
+        'Host': 'www.dbbqb.com',
+        'Referer': 'https://www.dbbqb.com/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61',
+        'Web-Agent': 'web',
+    }
+    response = requests.get(url=url, headers=headers).json()
+    for data in response:
+        img_url = 'https://image.dbbqb.com/' + data['path']
+        img_data = requests.get(url=img_url).content
+        title = img_url.split('/')[-1]
+        f = open('逗比图库\\' + title + '.jpg', 'wb')
+        f.write(img_data)
+        f.close()
+        print('---' + title + '已写入逗比图库---')
